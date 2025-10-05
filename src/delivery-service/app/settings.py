5ad0721277a2723a1924e687cf7fd8cfb2d40091
Doodlinka <oleshko.darya@gmail.com>
2025-10-05 18:02:12 +0300

    make json the only thing the API returns with debug = False

--- src/delivery-service/app/settings.py
+++ src/delivery-service/app/settings.py
@@ -44,7 +44,11 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-	'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',  # ✅ critical line
+	'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_RENDERER_CLASSES': (
+        ['rest_framework.renderers.JSONRenderer'] +
+        (['rest_framework.renderers.BrowsableAPIRenderer'] if DEBUG else [])
+    )
 }
 
 SPECTACULAR_SETTINGS = {
