142a1fd823d58a6aad2f12fb2b5ad2aa2d79792c
Dariya Omelchenko <dariyaomelchenko25@gmail.com>
2025-10-26 00:51:40 +0300

    Initial commit of frontend project

---
+++ src/pages/AdminReports.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AdminReports() {
+  return (
+    <div>
+      <h2>Звіти по продажах </h2>
+      <p>Цей розділ знаходиться у розробці.</p>
+      {/* Тут буде логіка для FR-054 */}
+    </div>
+  );
+}
+
+export default AdminReports;
