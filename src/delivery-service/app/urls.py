ec05e8c197cf1f1005775faf0b8b65328282e523
Doodlinka <oleshko.darya@gmail.com>
2025-10-26 14:14:37 +0200

    delete web views

--- src/delivery-service/app/urls.py
+++ src/delivery-service/app/urls.py
@@ -30,7 +30,4 @@ urlpatterns = [
     # Tokens (not versioned)
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    # Web pages
-    path("", include("restaurant.urls.web", namespace="restaurant")),
-    path("", include("accounts.web_urls", namespace="accounts")),
 ]
