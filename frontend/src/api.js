e984a08fef6fd4e04134d00e5732969ee0d60775
Doodlinka <oleshko.darya@gmail.com>
2025-10-29 14:32:36 +0200

    create initial manager, fix dev config

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,6 +3,7 @@
 import axios from 'axios';
 
 // 1. Create the base axios instance
+console.log(import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v0/')
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v0/'
 });
