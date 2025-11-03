870c1b0221a7b898448869984b5f29434ec14db7
Doodlinka <oleshko.darya@gmail.com>
2025-11-03 19:30:03 +0200

    use apiClient to login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './LoginPage.css'; 
-import axios from 'axios';
+import apiClient from '../api';
+import { toast } from "react-toastify";
 
 function LoginPage() {
   const [username, setUsername] = useState('');
@@ -15,29 +16,20 @@ function LoginPage() {
     console.log('Надсилаємо дані на сервер:', username, password);
 
     try {
-      // 1. Надсилаємо POST-запит на ваш Django ендпоінт
-      const response = await axios.post('http://localhost:8000/api/token/', {
-        // Ми припускаємо, що ваш Django налаштований на вхід по 'email'.
-        // Якщо він очікує 'username', змініть 'email' на 'username'.
+      const response = await apiClient.post('/api/token/', {
         username: username, 
         password: password
       });
-
-      // 2. Якщо запит успішний (код 200), Django поверне токени
-      console.log('Сервер відповів:', response.data);
       
-      // 3. Зберігаємо ОБИДВА токени у localStorage
       localStorage.setItem('accessToken', response.data.access);
       localStorage.setItem('refreshToken', response.data.refresh);
 
-      // 4. Повідомляємо користувача та перенаправляємо його
-      alert('Вхід успішний! Токени збережено.');
+      toast.success('Вхід успішний!');
       navigate('/admin/menu'); 
 
     } catch (error) {
-      // 5. Якщо сервер повернув помилку (400, 401, 500)
       console.error('Помилка входу:', error.response ? error.response.data : error.message);
-      alert('Неправильний username або пароль. Спробуйте ще раз.');
+      toast.error('Неправильний username або пароль. Спробуйте ще раз.');
     }
   };
 
