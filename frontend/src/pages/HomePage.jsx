0109c1e708be1b8f58413425b12ccd0107380899
Doodlinka <oleshko.darya@gmail.com>
2025-11-05 08:27:33 +0200

    make homepage use header component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,46 +1,11 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../Common.jsx';
 
 function HomePage() {
   return (
     <>
-      <header className="site-header">
-        <div className="container">
-          <a href="/" className="header-logo">
-            <img src="/content/Shopicons_Light_Stars.png" alt="Star-Logo" />
-            <h5>ClickEat</h5>
-          </a>
-
-          <nav className="header-navigation">
-            <ul>
-              <li><a href="/menu">Меню</a></li>
-              <li><a href="/faq">FAQ</a></li>
-            </ul>
-          </nav>
-
-          {/* <div className="header-search">
-            <img src="/content/Shopicons_Light_Search.png" alt="Search-Icon" />
-            <input type="text" placeholder="Пошук..." />
-          </div> */}
-
-          <div className="header-actions">
-            <button className="theme-toggle" aria-label="Перемкнути тему">
-              <img src="/content/Shopicons_Light_Sun.png" alt="Sun-Logo-Light_Theme" />
-            </button>
-
-            {/* <a href="/login" className="button button-secondary">
-              Log in <img src="/content/Shopicons_Light_Account.png" alt="" />
-            </a> */}
-
-            <a href="/cart" className="header-cart-button">
-              <span> <img src="/content/Shopicons_Light_Cart6.png" alt="" /></span>
-              <span className="cart-counter">0</span>
-            </a>
-          </div>
-        </div>
-      </header>
+      <Header />
       
-
       <main>
         <section className="hero" style={{ backgroundImage: "url(/content/pizza-bg.jpg)" }}>
           <div className="hero-overlay"></div>
