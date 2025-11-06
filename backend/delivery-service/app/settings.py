efd91a1c6b907fc618b918ab7d1ecb1aba352958
Anna <162580004+AnnaRomanchuk@users.noreply.github.com>
2025-11-06 14:22:37 +0200

    Update settings.py (test_secret)

--- backend/delivery-service/app/settings.py
+++ backend/delivery-service/app/settings.py
@@ -202,3 +202,4 @@ PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
     "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
 ]
+TEST_SECRET = os.getenv("TEST_SECRET")
